5116db1f0aee608f659bc437a596dbcf8e6943d7
Kurt Madel <kmadel@mac.com>
2015-06-02 13:14:11 -0400

    Update flow.groovy

--- flow.groovy
+++ flow.groovy
@@ -1,13 +1,14 @@
 node('jdk7') {
 
+	docker.image('maven:3.3.3-jdk-8').inside {
 	stage 'build'
 		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/apemberton/todo-web.git']]])
-		env.PATH="${tool 'mvn-3.2.2'}/bin:${env.PATH}"
 		sh 'mvn clean package'
 		archive 'target/*.war'
 
 	stage 'integration-test' 
 		sh 'mvn verify'
+	}
 }
 
 stage 'quality-and-functional-test'
